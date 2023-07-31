9ccb93211df9583a937b1449297dbb9ae00a8250
robat <r.tarabrin@outlook.de>
2023-07-31 23:22:52 +0200

    Replaced if-statement with switch case

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
 
 int main() {
 
-    sf::RenderWindow window(sf::VideoMode(1200, 800), "robat game");
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "robat game", sf::Style::Titlebar | sf::Style::Close);
     sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Red);
+    sf::Event evnt;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+    while (window.isOpen()) {
+
+        switch(window.pollEvent(evnt)) {
+
+            case sf::Event::Closed: 
                 window.close();
+                break; 
+            case sf::Event::KeyPressed:
+                if(evnt.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;    
+
         }
+
+        // Clears old frame
         window.clear();
+
+
         window.draw(shape);
+
+        // Tell app that window is done drawing
         window.display();
         
     }
